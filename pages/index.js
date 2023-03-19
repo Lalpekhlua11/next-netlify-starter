b06cb604567505bd8d6540f69a9f72e695a7f8e1
Lalpekhlua11 <102854272+Lalpekhlua11@users.noreply.github.com>
2023-03-20 03:20:35 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,67 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
-      <Footer />
-    </div>
-  )
+
+
+
+
+const loadingScreen = document.querySelector(".loading")
+
+const gameScreen = document.querySelector(".content")
+
+const loadingBar = document.querySelector(".loading .loading-bar")
+
+const loadingProg = document.querySelector(".loading .loading-bar .prog")
+
+const loadingText = document.querySelector(".loading .loading-text")
+
+const loadingTime = 270
+
+const loadingtxts = ["Loading", "Loading..", "loading Ressources..", "Loading files..", "Loading finishing..", "Loading complete.."]
+
+function load() {
+
+    let step = loadingTime / loadingtxts.length
+
+    let i = 0
+
+    let interval = setInterval(() => {
+
+        loadingProg.style.width = `${i * 100 / loadingtxts.length}%`
+
+        loadingText.innerText = loadingtxts[i]
+
+        i++
+
+        if (i >= loadingtxts.length) {
+
+            clearInterval(interval)
+
+            loadingProg.style.width = `100%`
+
+            setTimeout(() => {
+
+                loadingScreen.style.opacity = 0
+
+                gameScreen.style.display = "block"
+
+                gameScreen.style.opacity = 0
+
+                setTimeout(() => {
+
+                    gameScreen.style.opacity = 1
+
+                    loadingScreen.style.display = "none"
+
+                }, 300)
+
+            }, step)
+
+        }
+
+    }, step)
+
 }
+
+load() 
+
